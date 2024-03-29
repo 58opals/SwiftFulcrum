8b8707fc4f35d5251d4b71bf37d38e9fa4b79a7e
Coda Beatrix <coda@58opals.com>
2024-03-29 12:50:13 +0900

    Add a sample unit test for requestRelayFee method as a practice

--- Sources/SwiftFulcrum/SwiftFulcrum.swift
+++ Sources/SwiftFulcrum/SwiftFulcrum.swift
@@ -1,3 +1,14 @@
-struct SwiftFulcrum {
+public struct SwiftFulcrum {
     let client: Client
+    
+    func requestRelayFee() async throws -> Double {
+        let id = try await client.sendRequest(from: .blockchain(.relayFee))
+        
+        let waitingTime = 5.0
+        try await Task.sleep(nanoseconds: UInt64(waitingTime * 1_000_000_000.0))
+        
+        guard let relayFee = try client.jsonRPC.storage.result.blockchain.relayFee.getResult(for: id) else { fatalError() }
+        
+        return relayFee.fee
+    }
 }
--- Tests/SwiftFulcrumTests/SwiftFulcrumTests.swift
+++ Tests/SwiftFulcrumTests/SwiftFulcrumTests.swift
@@ -2,7 +2,12 @@ import XCTest
 @testable import SwiftFulcrum
 
 final class SwiftFulcrumTests: XCTestCase {
-    func testExample() throws {
+    let fulcrum = SwiftFulcrum(client: .init(webSocket: .init(url: .init(string: "wss://cashnode.bch.ninja:50004")!)))
+    
+    func testRelayFee() async throws {
+        let relayFee = try await fulcrum.requestRelayFee()
         
+        let expectedValue = 1e-5
+        XCTAssertEqual(relayFee, expectedValue, accuracy: 1e-5, "The relay fee did not match the expected value.")
     }
 }
