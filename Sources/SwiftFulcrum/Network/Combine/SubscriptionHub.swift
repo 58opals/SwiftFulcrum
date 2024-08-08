99cbaa525f6d179758e64a934cd8dd1975198950
Coda Beatrix <coda@58opals.com>
2024-08-09 00:32:04 +0900

    Adjusted `add`, `cancel`, and `cancelAll` functions to public

--- Sources/SwiftFulcrum/Network/Combine/SubscriptionHub.swift
+++ Sources/SwiftFulcrum/Network/Combine/SubscriptionHub.swift
@@ -4,16 +4,16 @@ import Combine
 public final class SubscriptionHub {
     private var subscriptions = [UUID: AnyCancellable]()
     
-    func add(_ cancellable: AnyCancellable, for identifier: UUID) {
+    public func add(_ cancellable: AnyCancellable, for identifier: UUID) {
         subscriptions[identifier] = cancellable
     }
     
-    func cancel(for identifier: UUID) {
+    public func cancel(for identifier: UUID) {
         subscriptions[identifier]?.cancel()
         subscriptions.removeValue(forKey: identifier)
     }
     
-    func cancelAll() {
+    public func cancelAll() {
         subscriptions.values.forEach { $0.cancel() }
         subscriptions.removeAll()
     }
