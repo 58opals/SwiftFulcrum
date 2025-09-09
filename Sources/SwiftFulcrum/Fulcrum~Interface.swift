14a389607aef177821510f3f59658e5fc1bb61c9
Coda Beatrix <coda@58opals.com>
2025-09-09 12:23:22 +0900

    2025-09-09-12-23-11

--- Sources/SwiftFulcrum/Fulcrum~Interface.swift
+++ Sources/SwiftFulcrum/Fulcrum~Interface.swift
@@ -5,18 +5,21 @@ import Foundation
 extension Fulcrum {
     public func submit<RegularResponseResult: JSONRPCConvertible>(
         method: Method,
-        responseType: RegularResponseResult.Type = RegularResponseResult.self
+        responseType: RegularResponseResult.Type = RegularResponseResult.self,
+        options: Client.Call.Options = .init()
     ) async throws -> RPCResponse<RegularResponseResult, Never> {
-        let (id, result): (UUID, RegularResponseResult) = try await client.call(method: method)
+        let (id, result): (UUID, RegularResponseResult) = try await client.call(method: method, options: options)
         return .single(id: id, result: result)
     }
     
     public func submit<SubscriptionNotification: JSONRPCConvertible>(
         method: Method,
-        notificationType: SubscriptionNotification.Type = SubscriptionNotification.self
+        notificationType: SubscriptionNotification.Type = SubscriptionNotification.self,
+        options: Client.Call.Options = .init()
     ) async throws -> RPCResponse<SubscriptionNotification, SubscriptionNotification> {
-        let token = Client.Call.Token()
-        let (id, initial, updates): (UUID, SubscriptionNotification, AsyncThrowingStream<SubscriptionNotification, Swift.Error>) = try await client.subscribe(method: method, options: .init(token: token))
+        let token = options.token ?? Client.Call.Token()
+        let effectiveOptions = Client.Call.Options(timeout: options.timeout, token: token)
+        let (id, initial, updates): (UUID, SubscriptionNotification, AsyncThrowingStream<SubscriptionNotification, Swift.Error>) = try await client.subscribe(method: method, options: effectiveOptions)
         return .stream(
             id: id,
             initialResponse: initial,
